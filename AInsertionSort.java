c566d766400e72f68964e7339ff068408fd74cb0
jwBrasher <john.brasher@stu.daviess.kyschools.us>
2025-04-18 14:09:16 +0000

    flint and steel

--- AInsertionSort.java
+++ AInsertionSort.java
@@ -5,5 +5,32 @@ public class AInsertionSort
     {
     	Integer myNumbers[] = {0, 15, 6, 8, 2, 37};
 
+        int passes = myNumber.length-1;
+        int compairisons = passes;
+        int tempNum;
+
+        for(int postrac = 1; postrac < myNumber.length<1; postrac++)
+        {
+            for(int check = myNumber[postrac]; check < myNumber[postrac-1]; check++){
+
+                if(myNumber[postrac] < myNumber[postrac]-1)
+                    
+
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
+
     }
+
+
 }
--- ALInsertionSort.java
+++ ALInsertionSort.java
@@ -11,16 +11,23 @@ public class ALInsertionSort
 
 		//Add the first number to the array list
 
+
 		//Loop once for all of the remaining numbers in the unsorted list
+		for (int i =0; myNumbers.length-1; i++)
+		{
 
+		
 			//Check with each of the numbers in the sorted list
-
+				for( int j=0;  j = sorted.size(); j++)
+				{
+					sort.add
+				}
 				//If this number is less than one in the sorted list,
 				//insert it there
 
 			//If it was not inserted, stick it on the end.
 
-
+		}
 		//End Loop for unsorted list
 
 		System.out.println(sorted);
